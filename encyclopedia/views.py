808009382aaa6276baae3e93c810a1904221ad6e
DamianDrozd-s29411 <s29411@pjwstk.edu.pl>
2024-08-03 12:05:04 +0200

    entry page added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -7,4 +8,11 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def title(request, name):
+    formated_text = markdown(util.get_entry(name))
+    return render(request, "encyclopedia/entry.html", {
+        "entries": formated_text
+    })
+
 

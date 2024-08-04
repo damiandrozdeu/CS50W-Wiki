52bf279855304681b9e1793fae7ad5f862742b53
DamianDrozd-s29411 <s29411@pjwstk.edu.pl>
2024-08-04 08:56:27 +0200

    Add Edit Page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.title, name ="title"),
+    path("wiki/<str:name>/", views.title, name ="title"),
     path("newpage", views.newpage, name ="newpage"),
     path("search", views.search, name ="search"),
     path("random", views.random_page, name ="random")
